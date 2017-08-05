7f7d5749aa6e49c6b21b5f62f91386e5b7177cbd
Jesu Vivek Vensuslaus <jesuvivek@Jesus-MacBook-Air.local>
2017-08-04 23:14:19 -0400

    Add postinstall script to build for browserify

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,10 +2,9 @@ import React, {Component} from 'react'
 
 export default class App extends Component {
   render(){
-    console.log('rendering')
     return(
       <div>
-        <h1>Hello World!</h1>
+        <h1>Hello Heroku!</h1>
       </div>
     )
   }
