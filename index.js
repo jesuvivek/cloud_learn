9b1f1377e5e05442afed3037cf40b8d9f38302df
Jesu Vivek Vensuslaus <jesuvivek@Jesus-MacBook-Air.local>
2017-08-05 19:34:55 -0400

    Add ESLint config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars')
 const app = express()
 
 const port = process.env.PORT || 3000
-app.use(express.static('public'));
+app.use(express.static('public'))
 app.engine('handlebars', exphbs.create().engine)
 app.set('view engine', 'handlebars')
 app.get('*', (req, res) => {
